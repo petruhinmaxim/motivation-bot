fee4bfd3dcdf37c0c341419adddc1377aa52e337
petruhinmaxim <petruhinmaxim@gmail.com>
2026-02-05 22:23:22 +0600

    recovery scene

--- src/scenes/start.scene.ts
+++ src/scenes/start.scene.ts
@@ -3,9 +3,13 @@ import type { Context } from 'grammy';
 import { BUTTONS, MESSAGES } from './messages.js';
 import { challengeService } from '../services/challenge.service.js';
 
+const SUPPORT_TELEGRAM_URL = 'https://t.me/petruhinMaks';
+
 export const startKeyboard = new InlineKeyboard()
   .text(BUTTONS.INFO, 'info')
-  .text(BUTTONS.BEGIN, 'begin');
+  .text(BUTTONS.BEGIN, 'begin')
+  .row()
+  .url(BUTTONS.RESTORE_CHALLENGE_DAYS, SUPPORT_TELEGRAM_URL);
 
 export async function handleStartScene(ctx: Context) {
   const userId = ctx.from?.id;
@@ -23,7 +27,9 @@ export async function handleStartScene(ctx: Context) {
     keyboard = new InlineKeyboard()
       .text(BUTTONS.TO_CHALLENGE, 'challenge_stats')
       .row()
-      .text(BUTTONS.INFO, 'info');
+      .text(BUTTONS.INFO, 'info')
+      .row()
+      .url(BUTTONS.RESTORE_CHALLENGE_DAYS, SUPPORT_TELEGRAM_URL);
   }
 
   // Если это callback query (нажатие на кнопку), редактируем сообщение
